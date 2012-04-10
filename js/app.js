25dbb09d9784b9daf7a85c89daa039f946644bfb
Marko Nikula <marko.nikula@relex.fi>
2012-04-10 14:06:36 +0300

    Implement missing features, add some new ones. Add comments for producing an annotated source page.

--- js/app.js
+++ js/app.js
@@ -1,63 +1,232 @@
+// The very first thing to do is to create the application namespace. This is done just like
+// normally when using Ember.js.
 var App = Em.Application.create();
 
+// ---
+
+// First we define a model class for Persons. Flame.Validatable is a mixin that allows defining
+// validations for properties of a model. When using Flame.FormView, those validations can then
+// be automatically checked and an error message shown when a property is not valid.
+App.Person = Ember.Object.extend(Flame.Validatable, {
+    validations: {
+        firstName: [Flame.Validator.notBlank],
+        lastName: [Flame.Validator.notBlank]
+    },
+
+    // This is an example of a computed property. Refer to the Ember.js documentation for more
+    // information about computed properties.
+    fullName: function() {
+        return '%@ %@'.fmt(this.get('firstName'), this.get('lastName'));
+    }.property('firstName', 'lastName').cacheable()
+});
+
+// ---
+
+// Next we define a controller for our application. In Flame/Ember, there's no need to use proxy
+// objects (like in SproutCore 1.x). Typically you create one controller for each model class.
+// You can think of this as a coordinating controller.
+App.personsController = Ember.Object.create({
+    // This array holds all persons. This can be seen to serve the function of a mediating controller.
+    // In a real app, this data would come from your data store.
+    all: [
+        App.Person.create({ firstName: 'Luke', lastName: 'Skywalker' }),
+        App.Person.create({ firstName: 'Obi-Wan', lastName: 'Kenobi' }),
+        App.Person.create({ firstName: 'Darth', lastName: 'Vader' }),
+        App.Person.create({ firstName: 'Han', lastName: 'Solo' }),
+        App.Person.create({ firstName: 'Master', lastName: 'Yoda' }),
+        App.Person.create({ firstName: 'Leia', lastName: 'Organa' })
+    ],
+    // This array holds all persons matching the current search text.
+    searchResults: null,
+    // This property holds the currently selected person.
+    selected: null,
+    // This property holds the current search text.
+    searchText: '',
+
+    init: function() {
+        this._super();
+        // Initialize results to all persons.
+        this.doSearch();
+    },
+
+    // An observer that performs the searching. We observe both the search text and the full name of each person.
+    // This way, it reacts also to changing person's name as well as deleting a person.
+    doSearch: function() {
+        var searchText = this.get('searchText');
+        var searchResults = this.get('all').filter(function(person) {
+            return !!person.get('fullName').match(searchText);
+        }).sort(function(person1, person2) {
+            return Ember.compare(person1.get('lastName'), person2.get('lastName'));
+        });
+        this.set('searchResults', searchResults);
+    }.observes('searchText', 'all.@each.fullName'),
+
+    createPerson: function() {
+        this.get('all').pushObject(App.Person.create({
+            firstName: 'New',
+            lastName: 'Person'
+        }));
+    },
+
+    // This function is called when user clicks the 'Delete' button.
+    confirmDelete: function() {
+        var selected = this.get('selected');
+        var self = this;
+        if (selected) {
+            // Flame.AlertPanel is a panel for displaying simple messages to user, optionally with some buttons
+            // like Cancel and OK. The #warn method is a helper that includes a warning icon in the dialog.
+            Flame.AlertPanel.warn({
+                title: 'Confirm Delete',
+                message: 'Are you sure you want to delete %@?'.fmt(selected.get('fullName')),
+                onConfirm: function() {
+                    this._super();
+                    self.deleteSelectedPerson();
+                }
+            }).popup();
+        }
+    },
+
+    // This function is called when user confirms the delete in the warning dialog.
+    deleteSelectedPerson: function() {
+        var selected = this.get('selected');
+        this.set('selected', null);
+        this.get('all').removeObject(selected);
+    }
+
+});
+
+// ---
+
+// This is the root view of the application. You can make it shown initially by either using a
+// handlebars template in the index.html, as done in this application, or by calling its
+// append method upon initializing your application.
 App.RootView = Flame.RootView.extend({
+    // The view covers the entire screen, as is usually the case with the root view.
     layout: { top: 0, left: 0, right: 0, bottom: 0 },
     childViews: 'splitView'.w(),
 
+    // A horizontal split view divides the view in two, showing a draggable separator between the two parts.
     splitView: Flame.HorizontalSplitView.extend({
         leftWidth: 250,
         minLeftWidth: 200,
 
+        // Split view always has to have child views named 'leftView' and 'rightView'. You don't need to
+        // define the childViews array yourself for a split view.
         leftView: Flame.View.extend({
             childViews: 'titleView searchFieldView listView'.w(),
 
+            // This is the bar at the top of the left view, showing the buttons and the application title.
             titleView: Flame.View.extend({
                 layout: { height: 35, left: 0, right: 0, top: 0 },
                 classNames: ['title'],
-                childViews: 'labelView addButtonView'.w(),
+                childViews: 'aboutButtonView labelView addButtonView'.w(),
+
+                aboutButtonView: Flame.ButtonView.extend({
+                    acceptsKeyResponder: false,
+                    layout: { top: 5, left: 5, width: 25 },
+                    title: '?',
+                    // Generally it's recommended to write actions in controllers and keep views fully
+                    // declarative. But sometimes it makes sense to do this kind of very simple things
+                    // directly in the view code.
+                    action: function() {
+                        App.AboutPanel.create().popup();
+                    }
+                }),
 
                 labelView: Flame.LabelView.extend({
-                    layout: { left: 5, right: 5, top: 5 },
+                    layout: { left: 35, right: 35, top: 7 },
                     textAlign: Flame.ALIGN_CENTER,
                     value: 'Address Book'
                 }),
 
                 addButtonView: Flame.ButtonView.extend({
                     acceptsKeyResponder: false,
-                    layout: { top: 5, right: 10, width: 25 }
+                    layout: { top: 5, right: 5, width: 25 },
+                    title: '+',
+                    // You can define the target either as a binding or as a literal. Either way, make sure
+                    // the controller has been defined when this is evaluated (usually you want to load all
+                    // controllers before views).
+                    target: App.personsController,
+                    action: 'createPerson'
                 })
             }),
 
             searchFieldView: Flame.SearchTextFieldView.extend({
                 layout: { top: 42, left: 5, right: 5 },
-                placeholder: 'Search'
+                placeholder: 'Search',
+                // Here we use a binding to bind the value of the text field to a property in the controller.
+                // The property is updated live when user types in the field. The controller has an observer
+                // that reacts to changes and updates the list of persons appropriately.
+                valueBinding: 'App.personsController.searchText'
             }),
 
+            // List view is a view that creates a child view for each item in its content array.
             listView: Flame.ListView.extend({
                 layout: { top: 70, left: 0, right: 0 },
-                contentBinding: 'App.peopleController.content',
-                selectionBinding: 'App.personController.content',
+                contentBinding: 'App.personsController.searchResults',
+                selectionBinding: 'App.personsController.selected',
+                // This is the view class that is used for the child views. It should always be a subclass
+                // of Flame.ListItemView.
                 itemViewClass: Flame.ListItemView.extend({
-                    handlebars: "&nbsp;&nbsp;{{content.fullName}}"
+                    // Any Flame view can either define its contents through nested child views or an embedded
+                    // handlebars template. Using a template is a very handy way to produce dynamic text.
+                    handlebars: "{{content.fullName}}"
                 })
             })
         }),
 
+        // This is the right side of the split view, showing the details of the selected person.
         rightView: Flame.View.extend({
-            childViews: 'formView'.w(),
+            // Here we use a VerticalStackLayoutManager to manage the layout of this view. It will automatically
+            // place the child views on top of each other, based on the height of each child.
+            layoutManager: Flame.VerticalStackLayoutManager.create({
+                topMargin: 5,
+                spacing: 5,
+                bottomMargin: 5
+            }),
+            layout: { top: 20, left: 20, right: 20 },
+            // This binding uses a transform so that the resulting value is 'true' when no person has been selected,
+            // hiding this view.
+            isVisibleBinding: Ember.Binding.from('App.personsController.selected').isNull().not(),
+            childViews: 'formView deleteButtonView'.w(),
 
+            // Flame.FormView is a helper that dynamically creates a number of fields with associated labels.
+            // It's also able to show validation messages if the value of a field is not valid.
             formView: Flame.FormView.extend({
-                layout: { top: 20, left: 20, right: 20, bottom: 20 },
-                objectBinding: 'App.personController.content',
-                isVisibleBinding: Ember.Binding.from('App.personController.content').isNull().not(),
+                objectBinding: 'App.personsController.selected',
                 labelWidth: 80,
                 controlWidth: 200,
                 properties: [
-                    { property: 'firstName', label: 'First name', validation: 'Please provide a first name' },
-                    { property: 'lastName', label: 'Last name', validation: 'Please provide a last name' }
+                    { property: 'firstName', label: 'First name',
+                      validation: 'Please provide a first name' },
+                    { property: 'lastName', label: 'Last name',
+                      validation: 'Please provide a last name' }
                 ]
+            }),
+
+            deleteButtonView: Flame.ButtonView.extend({
+                layout: { left: 230, width: 80 },
+                title: 'Delete',
+                target: 'App.personsController',
+                action: 'confirmDelete'
             })
         })
     })
 });
 
+// ---
+
+// This panel that is shown when user clicks the '?' button.
+App.AboutPanel = Flame.Panel.extend({
+    layout: { width: 350, height: 160, centerX: 0, centerY: -50 },
+    title: 'Flame.js Example Application',
+    allowMoving: true,
+
+    // A Flame.Panel should always have exactly one child view, called 'contentView'.
+    contentView: Flame.View.extend({
+        layout: { top: 50, left: 20, right: 20, height: 150 },
+        // Embedded handlebars templates are also handy for producing bits of HTML markup.
+        handlebars: 'This is an example application for '+
+            '<a href="https://github.com/flamejs/flame.js" target="_blank">Flame.js</a>.'
+    })
+});
